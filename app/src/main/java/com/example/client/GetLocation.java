00fe0e2fb434cb74e0a0b51ee0535e448d08e48d
Anirudh Pupneja <apupneja2002@gmail.com>
2022-10-12 15:36:39 +0530

    Added data.csv and created filehandler for a single user

---
+++ app/src/main/java/com/example/client/GetLocation.java
@@ -0,0 +1,65 @@
+package com.example.client;
+
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+
+import com.opencsv.CSVReader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class GetLocation {
+    //get the longitude and latitude from the file
+
+    private double longitude;
+    private double latitude;
+    private int id;
+    private String nextLine;
+
+    private BufferedReader reader;
+    public GetLocation(Context context){
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(context.getAssets().open("data.csv")));
+            reader.readLine();
+        } catch (IOException e) {
+            Log.d("GetLocation", e.getMessage());
+        }
+    }
+
+    public void getData(){
+        try {
+            nextLine=reader.readLine();
+            while (nextLine!= null) {
+                // nextLine[] is an array of values from the line
+                String[] row = nextLine.split(",");
+                Log.i("check", nextLine);
+                longitude = Double.parseDouble(row[0]);
+                latitude = Double.parseDouble(row[1]);
+                id = Integer.parseInt(row[2]);
+
+                nextLine = reader.readLine();
+                break;
+            }
+        } catch (IOException e) {
+            Log.d("GetLocation", e.getMessage());
+        }
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
--- app/src/main/java/com/example/client/MainActivity.java
+++ app/src/main/java/com/example/client/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.client;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button send_location, send_location_simulator;
+    private TextView current_status;
+
+    private int pressed = 0, pressedSimulator = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        send_location = findViewById(R.id.send_location);
+        current_status = findViewById(R.id.current_status);
+        //send_location_simulator = findViewById(R.id.send_location_simulator);
+
+        send_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(pressed == 0){
+                    pressed = 1;
+                    current_status.setText("Sending location...");
+                    send_location.setBackgroundColor(getResources().getColor(R.color.purple_500));
+                    GetLocation getLocation = new GetLocation(MainActivity.this);
+
+                    for(int i=0;i<10;i++){
+                        getLocation.getData();
+                        Log.d("Location",Integer.toString(getLocation.getId())+" "+Double.toString(getLocation.getLatitude())+" "+Double.toString(getLocation.getLongitude()));
+                    }
+                }
+                else if(pressed == 1){
+                    pressed = 0;
+                    current_status.setText("Stopped sending location");
+                    send_location.setBackgroundColor(getResources().getColor(R.color.purple_200));
+                }
+            }
+        });
+
+
     }
 }
